c5661d584b2fb477afd5e4dd799700f573ca3446
greemfox <pavelkurchanov.lpk@gmail.com>
2026-08-17 15:27:27 +0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pkurchanov/convrrt
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+// TODO:
+// [ ] webpage
+// [ ] submit form to server
+// [ ] convert value
+// [ ] display value
+
+func main() {
+	fmt.Println("hi")
+}
